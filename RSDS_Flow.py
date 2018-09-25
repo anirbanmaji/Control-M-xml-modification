1f8504c14f4e71fc0a83aa51f44f36659693d423
Anirban Maji <anmaji94@in.ibm.com>
2018-09-25 07:50:41 +0530

    flow testing

--- RSDS_Flow.py
+++ RSDS_Flow.py
@@ -2,79 +2,99 @@ import xml.etree.ElementTree as ET
 from createJob import createJob
 from createConnection import buildSimpleConnection
 
-extension = ('ac','cc','sc','pc','fp')
-extSbt = ('ac','cc','sc','pc')
-flow = [[{'AR':extension}],[{'SBT':extSbt}],[{'FW':extension}],[{'DLT':extension}],[{'CP':extension}],[{'LD':extension}],[{'TV':extension},{'DV':extension},{'EBT':extSbt}]]
 
 
-def createSingleFlowTemplate(appCode, countryCode, serverCode, projectCode, filePattern, flow): #add 'effectiveAppCode' and 'srcSys'
+def createSingleFlowTemplate(appCode, countryCode, effectiveAppCode, srcSys, projectCode, filePattern, server, flow): #add 'effectiveAppCode' and 'srcSys'
     
     '''define 'folderName' variable here and then pass it while creating jobs and folder'''
+    ''''BIHZZ_P_EWPSGL_APRA_ABC'''
     
     jobNameList = []
+    levelwiseJobs = []
+    levelwiseJobsExt = []
+    
     jobBufferBeforeTree = []
-    for layer in flow:
-        #list
-        for node in layer:
-            sameLevelJobs = []
-            #dictionary
-            for actExt in node:
+
+    serverToCode = {'PCMAU01':'P', 'DCMAU02':'T2', 'DCMAU01':'P'}
+
+    serverCode = []
+    serverCode.append(serverToCode[server])
+    
+    for layer in flow: #layer is a list of dict
+        sameLevelJobs = []
+        sameLevelJobsNames = []
+        sameLevelJobsNamesWithoutAct = []
+        
+        for node in layer: #node is a dict
+            for actExt in node: #actExt is a key of dictionary
                 #string
                 #print(node[actExt])
                 extList = node[actExt]
+
+                '''The following part is used only for job name'''
                 for ac in appCode:
                     for cc in countryCode:
                         for sc in serverCode:
-                            for pc in projectCode:
-                                for fp in filePattern:
-                                    jobName = (ac if ('ac' in extList) else '')+(cc if 'cc' in extList else '')+('_'+sc if 'sc' in extList else '')+('_'+pc if 'pc' in extList else '')+('_'+fp if 'fp' in extList else '')+'_'+actExt
-                                    jobNameWithoutAct = (ac if ('ac' in extList) else '')+(cc if 'cc' in extList else '')+('_'+sc if 'sc' in extList else '')+('_'+pc if 'pc' in extList else '')+('_'+fp if 'fp' in extList else '')
-                                    currentJob = createJob(JOBNAME=jobName)
-                                    currentJobName = currentJob.attrib['JOBNAME']
-                                    sameLevelJobsNames = []
-                                    for element in sameLevelJobs:
-                                        sameLevelJobsNames.append(element.attrib['JOBNAME'])
-                                    if currentJobName not in sameLevelJobsNames:
-                                        sameLevelJobs.append(currentJob)
-                                        try:
-                                            #print(jobNameList[-1])
-                                            spread = True
-                                            for prevJob in jobNameList[-1]:#to connect to the same extesion jobs, one-to-one and many-to-one connection 
-                                                if jobNameWithoutAct in prevJob.attrib['JOBNAME']:#check if previous jobs extension is matching with the current jobs
-                                                    #print('True')
-                                                    #print('pre '+prevJob.attrib['JOBNAME'])
-                                                    spread = False
-                                                    indexOfPreviousJob = jobBufferBeforeTree.index(prevJob)
-                                                    jobBufferBeforeTree[indexOfPreviousJob],currentJob = buildSimpleConnection(prevJob, currentJob)#make a function like buildConnection(predessor, succesor), more details in Sublimetext
-                                            if spread:#one-to-many connection
-                                                #print('spread')
-                                                prevJob = jobNameList[-1][0]
-                                                indexOfPreviousJob = jobBufferBeforeTree.index(prevJob)
-                                                jobBufferBeforeTree[indexOfPreviousJob],currentJob = buildSimpleConnection(prevJob, currentJob)
-                                                #print('pre '+prevJob.attrib['JOBNAME'])
-                                                #buildConnection(jobNameList[-1][0], jobName)
-                                        except:
-                                            pass
-                                        print('current '+currentJob.attrib['JOBNAME'])
-                                        jobBufferBeforeTree.append(currentJob)
-            jobNameList.append(sameLevelJobs)
-            #print(jobNameList)
-            print()
+                            for eac in effectiveAppCode:
+                                for ss in srcSys:
+                                    for pc in projectCode:
+                                        for fp in filePattern:
+                                            jobNameWithoutAct = (ac if ('ac' in extList) else '')+(cc if 'cc' in extList else '')+('_'+sc if 'sc' in extList else '')+('_'+eac if 'eac' in extList else '')+(ss if 'ss' in extList else '')+('_'+pc if 'pc' in extList else '')+('_'+fp if 'fp' in extList else '')
+                                            jobName = jobNameWithoutAct+'_'+actExt
+                                            currentJob = createJob(JOBNAME=jobName)
+                                            if jobName in sameLevelJobsNames:
+                                                pass
+                                            else:
+                                                sameLevelJobs.append(currentJob)
+                                                sameLevelJobsNames.append(jobName)
+                                                sameLevelJobsNamesWithoutAct.append(jobNameWithoutAct)
+                                            #print(jobName)
+
+        levelwiseJobs.append(sameLevelJobs)
+        levelwiseJobsExt.append(sameLevelJobsNamesWithoutAct)
+
+    for p in levelwiseJobs:
+        for q in p:
+            print(q.attrib["JOBNAME"])
+        print()
+    print('________________________________________')
+
+    
+    for i in range(1, len(levelwiseJobs)):
+        for j in range(0,len(levelwiseJobs[i])):
+            print(levelwiseJobsExt[i][j])
+            currentJobExt = levelwiseJobsExt[i][j]
+            try:
+                indexOfPrevJob = levelwiseJobsExt[i-1].index(currentJobExt)
+                prevJob = levelwiseJobs[i-1][indexOfPrevJob]#this will work only if same job extension exist for the previous job
+                currentJob = levelwiseJobs[i][j]
+                levelwiseJobs[i-1][indexOfPrevJob],levelwiseJobs[i][j] = buildSimpleConnection(prevJob, currentJob)
+            except:
+                print('Branching or marging')
+                for k in range(0,len(levelwiseJobs[i-1])):
+                    prevJob = levelwiseJobs[i-1][k]
+                    currentJob = levelwiseJobs[i][j]
+                    levelwiseJobs[i-1][k],levelwiseJobs[i][j] = buildSimpleConnection(prevJob, currentJob)
+
+    for p in levelwiseJobs:
+        for q in p:
+            jobBufferBeforeTree.append(q)
+         
     root = ET.Element('DEFTABLE')       
     folderName = jobBufferBeforeTree[0].attrib['PARENT_FOLDER']
-    folder = ET.Element('FOLDER', DATACENTER="DCMAU01", PARENT_FOLDER=folderName)
+    folder = ET.Element('FOLDER', DATACENTER=server, PARENT_FOLDER=folderName)
     root.append(folder)
     tree = ET.ElementTree(root)
     for job in jobBufferBeforeTree:
         folder.append(job)
     return tree
 
-##job1 = createJob(JOBNAME = 'JOB1')
-##job2 = createJob(JOBNAME = 'JOB2')
-##x,y = buildSimpleConnection(job1, job2)
-##print(ET.tostring(x))
-##print(ET.tostring(y))
 
-modifiedFile = "new.xml"
-createSingleFlowTemplate(['BIH'],['ZZ'],['P'],['PSGL'],['ABC','DEF'], flow).write(modifiedFile)
+if __name__ == "__main__":
+    extension = ('ac','cc','sc','eac','ss','pc','fp')
+    extSbt = ('ac','cc','sc','eac','ss','pc')
+    flow = [[{'AR':extension}],[{'SBT':extSbt}],[{'FW':extension}],[{'DLT':extension}],[{'CP':extension}],[{'LD':extension}],[{'TV':extension},{'DV':extension},{'EBT':extSbt}]]
+    modifiedFile = "new.xml"
+    server = "PCMAU01"
+    createSingleFlowTemplate(['BIH'],['ZZ'],['EW'],['PSGL'],['APRA'],['ABC','DEF'],server, flow).write(modifiedFile)
 
--- commandLineRecog.py
+++ commandLineRecog.py
@@ -1,5 +1,6 @@
 import openpyxl
 from getValuesFromHeader import getDataofHeader
+from RSDS_Flow import createSingleFlowTemplate
 
 xlFile = 'C:/Users/AnirbanMaji/Desktop/Control M RSDS jobs/RSDS_SITControl M_requirement_sheet_MYTELL.xlsm'
 cmdHeader = 'Command Line'
@@ -9,10 +10,21 @@ sheet = 'MYTELL_AU'
 allCommands = [x for x in getDataofHeader(xlFile, sheet, cmdHeader) if x!=None] #this odd looking code for removing none values
 allJobNames = [y for y in getDataofHeader(xlFile, sheet, jbNameHeader) if y!=None]
 
-for i in allCommands:
-    print(i)
-    print()
 
-for j in allJobNames:
-    print(j)
-    print()
+
+
+extension = ('ac','cc','sc','eac','ss','pc','fp')
+extSbt_and_Ebt = ('ac','cc','sc','eac','ss','pc')
+flow = [[{'AR':extension}],[{'SBT':extSbt_and_Ebt}],[{'FW':extension}],[{'DLT':extension}],[{'CP':extension}],[{'LD':extension}],[{'TV':extension},{'DV':extension},{'EBT':extSbt_and_Ebt}]]
+modifiedFile = "RSDS_SITControl M_requirement_sheet_MYTELL.xml"
+server = "PCMAU01"
+'''createSingleFlowTemplate(appCode, countryCode, effectiveAppCode, srcSys, projectCode, filePattern, server, flow)'''
+createSingleFlowTemplate(['BIH'],['AU'],['EW'],[''],['MYTRELL'],['EJTABLE1','PINPADLOG', 'SECURITY'],server, flow).write(modifiedFile)
+'''pass the xmlTree and all the commandlines to a function that will modify the tree by adding the correct command lines'''
+##for i in allCommands:
+##    print(i)
+##    print()
+##
+##for j in allJobNames:
+##    print(j)
+##    print()
